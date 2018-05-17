a34e72a391dbeee1553d3bf44393c87707cd6fb3
Ana Zegheanu <ana.zegheanu@gmail.com>
2018-05-17 13:55:29 +0200

    first add

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,51 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require activestorage
+//= require turbolinks
+//= require_tree .
+//= require jquery.min
+//= require popper.min
+//= require bootstrap.min
+//= require slidebar
+//= require classie
+//= require bootstrap-growl.min
+//= require scrollpos-styler
+//= require adaptive-backgrounds
+//= require jquery.countdown.min
+//= require dropzone.min
+//= require jquery.easypiechart.min
+//= require jquery.fancybox.min
+//= require flatpickr.min
+//= require flip.min
+//= require footer-reveal.min
+//= require jquery.gradientify.min
+//= require headroom.min
+//= require jquery.headroom.min
+//= require input-mask.min
+//= require instafeed
+//= require jquery.countTo
+//= require nouislider.min
+//= require paraxify.min
+//= require select2.min
+//= require sticky-kit.min
+//= require swiper.min
+//= require autosize.min
+//= require typeahead.bundle.min
+//= require typed.min
+//= require vide.min
+//= require viewportchecker.min
+//= require wow.min
+//= require isotope.min
+//= require imagesloaded.pkgd.min 
+//= require boomerang.min
